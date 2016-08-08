141b4eacadc3ef1c012aecd047bca94e111e705b
moriyama1x4 <moriyama.1x4@gmail.com>
2016-08-08 15:14:59 +0900

    201608081514

--- hoihoi/chakuriku.js
+++ hoihoi/chakuriku.js
@@ -7,64 +7,64 @@ var winLose = 2; //勝敗判定値
 
 //ボールを座標指定
 function setPosition(id, x, y) {
-	document.getElementById(id).style.left = x;
-	document.getElementById(id).style.top  = y;		  
+    document.getElementById(id).style.left = x;
+    document.getElementById(id).style.top  = y;		  
 }
 
 //ボールのX座標取得
 function getX(id){
-	return parseInt(document.getElementById(id).style.left) ;
+    return parseInt(document.getElementById(id).style.left) ;
 }
 
 //ボールのY座標取得
 function getY(id){
-	return parseInt(document.getElementById(id).style.top);
+    return parseInt(document.getElementById(id).style.top);
 }
 
 //ボールを動かす
 function moveBall(x, y){
-	ballX = getX("blueBall");
-	ballY = getY("blueBall");
-	
-	ballX += x;
-	ballY += y;
-	setPosition("blueBall", ballX, ballY);
+    var ballX = getX("blueBall");
+    var ballY = getY("blueBall");
+
+    ballX += x;
+    ballY += y;
+    setPosition("blueBall", ballX, ballY);
 }
 
 function gameBody() {
-	moveBall(0,velo);
-	if(getY("blueBall") < 400){
-		velo += accel;
-		gameStart();
-	}
-	else{
-		if(velo < winLose){
-			alert("着陸成功！(時速" + velo + "km)");
-		}
-		else{
-			alert("着陸失敗・・・(時速" + velo + "km)");
-		}
-	}
+    moveBall(0,velo);
+    if(getY("blueBall") < 400){
+        velo += accel;
+        gameStart();
+    }
+    else{
+        if(velo < winLose){
+            alert("着陸成功！(時速" + velo + "km)");
+        }
+        else{
+            alert("着陸失敗・・・(時速" + velo + "km)");
+        }
+    }
 }
 
 //開始
 function gameStart(){
-	timerID = setTimeout("gameBody()", 50);
+    timerID = setTimeout("gameBody()", 50);
 }
 
 //停止
 function gameStop(){
-	clearTimeout(timerID);
+    clearTimeout(timerID);
 }
 
 //減速
 function braking(){
-	velo -= brake;
+    velo -= brake;
 }
 
 //リセット
 function reset(){
-	clearTimeout(timerID);
-	setPosition("blueBall", defPosition[0], defPosition[1]);
-	velo = defVelo;
+    clearTimeout(timerID);
+    setPosition("blueBall", defPosition[0], defPosition[1]);
+    velo = defVelo;
 }
