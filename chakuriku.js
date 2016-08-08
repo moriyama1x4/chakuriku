680866062a1a3498293eea389828b95ed2dfecd8
moriyama1x4 <moriyama.1x4@gmail.com>
2016-08-08 22:18:51 +0900

    201608082218

--- chakuriku.js
+++ chakuriku.js
@@ -1,9 +1,15 @@
 ﻿var defPosition = [128,8]; //初期位置
-var defVelo = 1; //初速度
+var defVelo = 1; //デフォルト速度
 var velo = defVelo; //速度
-var accel = 0.25; //加速度
+var defAccel = 0.1; //デフォルト加速度
+var accel = defAccel; //加速度
 var brake = 1; //減速度
-var winLose = 2; //勝敗判定値
+var defWinLose = 6; //デフォルト勝敗判定値
+var winLose = defWinLose; //勝敗判定値
+var defLevel = 1; //デフォルトレベル
+var maxLevel = 5; //最大レベル
+var level = defLevel; //レベル
+var timer;　//タイマー
 
 //ボールを座標指定
 function setPosition(id, x, y) {
@@ -35,26 +41,40 @@ function gameBody() {
     moveBall(0,velo);
     if(getY("blueBall") < 400){
         velo += accel;
-        gameStart();
     }
     else{
+        console.log("level = " + level);
+        console.log("accel = " + accel);
+        console.log("winLose = " + winLose)
         if(velo < winLose){
-            alert("着陸成功！(時速" + velo + "km)");
+            if(level >= maxLevel){
+                alert("着陸成功！(時速" + (Math.round(velo * 10) / 10) + "km)。 最大レベルクリア！");
+                reset();
+            }
+            else{
+                alert("着陸成功！(時速" + (Math.round(velo * 10) / 10) + "km)。 レベル"　+ (level+1) + "にチャレンジ！");
+                level ++;
+                accel = defAccel + ((level - defLevel) * 0.05);
+                winLose = defWinLose - ((level - defLevel) * 1);
+                document.getElementById("level-num").innerHTML=level
+            }
         }
         else{
-            alert("着陸失敗・・・(時速" + velo + "km)");
+            alert("着陸失敗・・・(時速" + (Math.round(velo * 10) / 10) + "km)。 またレベル1からチャレンジ！");
+            reset();
         }
+        restart();
     }
 }
 
 //開始
 function gameStart(){
-    timerID = setTimeout("gameBody()", 50);
+    timer = setInterval("gameBody()", 50);
 }
 
 //停止
 function gameStop(){
-    clearTimeout(timerID);
+    clearInterval(timer);
 }
 
 //減速
@@ -62,9 +82,17 @@ function braking(){
     velo -= brake;
 }
 
-//リセット
-function reset(){
-    clearTimeout(timerID);
+//リスタート
+function restart(){
+    clearInterval(timer);
     setPosition("blueBall", defPosition[0], defPosition[1]);
     velo = defVelo;
+}
+
+//リセット
+function reset(){
+    level = defLevel;
+    accel = defAccel;
+    winLose = defWinLose;
+    document.getElementById("level-num").innerHTML=defLevel;
 }
